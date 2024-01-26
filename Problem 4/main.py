3f8c3ee29005ba9a3cd10ec42c0bb920b5c68237
JelleWierenga <116724275+JelleWierenga@users.noreply.github.com>
2024-01-26 15:22:23 +0100

    add solution challange 4

--- Problem 4/main.py
+++ Problem 4/main.py
@@ -4,10 +4,12 @@ Problem Statement: A palindromic number reads the same both ways. The largest pa
 
 num1 = 100
 num2 = 100
-
-for i in range(num1 + num2):
-    num1, num2 = (num1, num2 + 1) if num1 > num2 else (num1 + 1, num2)
-    calc = num1 * num2
-    if str(calc) == str(calc)[::-1]:
-        print(f"{num1} * {num2} = {calc}")
-        break
+biggest = None
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        calc = i * j
+        if str(calc) == str(calc)[::-1]:
+            num1 = i
+            num2 = j
+            biggest = calc if biggest is None or calc > biggest else biggest
+print(f"{num1} * {num2} = {biggest}")
